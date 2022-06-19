18a045f7afd40b00e197082f236f255fbcea1bb4
chizakasipascal <pascalkasichiza@gmail.com>
2022-06-20 00:54:13 +0200

    profilr fix logic bug uxix

--- lib/src/views/screens/profile.dart
+++ lib/src/views/screens/profile.dart
@@ -14,69 +14,162 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return DefaultTabController(
-      length: 3,
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            const SliverAppBar(
-              expandedHeight: 400.0,
-              floating: false,
-              pinned: true,
-              backgroundColor: Colors.transparent,
+    return ListView(
+      controller: controller,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        const BackgroundProfil(),
+        const ProfileMoreActionButtom(),
+        const ProfileMoreActionButtom(
+          description: 'From ',
+          subdescritpion: 'Goma Town',
+          icon: FontAwesome.location_arrow,
+        ),
+        const ProfileMoreActionButtom(
+          description: 'Birtday ',
+          subdescritpion: 'December 25',
+          icon: FontAwesome.birthday_cake,
+        ),
+        Container(
+          height: 1,
+          color: kGreyColor,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+              bottom: 20.0, top: 20.0, left: 20, right: 20),
+          child: SizedBox(
+            height: 50,
+            width: 160,
+            child: MaterialButton(
               elevation: 0.0,
-              flexibleSpace: FlexibleSpaceBar(
-                // centerTitle: true,
-                // title: Text(
-                //   "@username",
-                //   style: themeData.textTheme.bodyText1,
-                // ),
-                background: BackgroundProfil(),
+              color: kGreyColor.withOpacity(.3),
+              onPressed: () {},
+              child: Text(
+                'Edit profile',
+                style: themeData.textTheme.bodyLarge!.copyWith(
+                  color: kDoniel,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            // SliverPersistentHeader(
-            //   delegate: _SliverAppBarDelegate(
-            //     const TabBar(
-            //       labelColor: kDoniel,
-            //       indicatorWeight: 1,
-            //       indicatorColor: kDoniel,
-            //       unselectedLabelColor: kGreyColor,
-            //       tabs: [
-            //         Tab(text: "Media"),
-            //         Tab(text: "Status"),
-            //         Tab(text: "Bio"),
-            //       ],
-            //     ),
-            //   ),
-            //   pinned: true,
-            // ),
-          ];
-        },
-        body: ListView(
-          controller: controller,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  const Icon(
-                    AntDesign.home,
-                    color: kGreyColor,
-                  ),
-                  Text(
-                    'Lives in ',
-                    style: themeData.textTheme.bodyText2,
-                  ),
-                  Text(
-                    'Goma.DRC',
-                    style:
-                        themeData.textTheme.bodyText2!.copyWith(color: kDoniel),
-                  ),
-                ],
-              ),
-            )
-          ],
+          ),
         ),
+      ],
+    );
+
+    // NestedScrollView(
+    //   headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+    //     return <Widget>[
+    //       const SliverAppBar(
+    //         expandedHeight: 400.0,
+    //         floating: false,
+    //         pinned: true,
+    //         backgroundColor: Colors.transparent,
+    //         elevation: 0.0,
+    //         flexibleSpace: FlexibleSpaceBar(
+    //           // centerTitle: true,
+    //           // title: Text(
+    //           //   "@username",
+    //           //   style: themeData.textTheme.bodyText1,
+    //           // ),
+
+    //           background: BackgroundProfil(),
+    //         ),
+    //       ),
+    //       // SliverPersistentHeader(
+    //       //   delegate: _SliverAppBarDelegate(
+    //       //     const TabBar(
+    //       //       labelColor: kDoniel,
+    //       //       indicatorWeight: 1,
+    //       //       indicatorColor: kDoniel,
+    //       //       unselectedLabelColor: kGreyColor,
+    //       //       tabs: [
+    //       //         Tab(text: "Media"),
+    //       //         Tab(text: "Status"),
+    //       //         Tab(text: "Bio"),
+    //       //       ],
+    //       //     ),
+    //       //   ),
+    //       //   pinned: true,
+    //       // ),
+    //     ];
+    //   },
+    //   body:
+    //   ListView(
+    //     controller: controller,
+    //     physics: const BouncingScrollPhysics(),
+    //     children: [
+    //       const ProfileMoreActionButtom(),
+    //       const ProfileMoreActionButtom(
+    //         description: 'From ',
+    //         subdescritpion: 'Goma Town',
+    //         icon: FontAwesome.location_arrow,
+    //       ),
+    //       const ProfileMoreActionButtom(
+    //         description: 'Birtday ',
+    //         subdescritpion: 'December 25',
+    //         icon: FontAwesome.birthday_cake,
+    //       ),
+    //       Container(
+    //         height: 1,
+    //         color: kGreyColor,
+    //       ),
+    //       Padding(
+    //         padding: const EdgeInsets.only(
+    //             bottom: 20.0, top: 20.0, left: 20, right: 20),
+    //         child: SizedBox(
+    //           height: 50,
+    //           width: 160,
+    //           child: MaterialButton(
+    //             elevation: 0.0,
+    //             color: kGreyColor.withOpacity(.3),
+    //             onPressed: () {},
+    //             child: Text(
+    //               'Edit profile',
+    //               style: themeData.textTheme.bodyLarge!.copyWith(
+    //                 color: kDoniel,
+    //                 fontWeight: FontWeight.bold,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+}
+
+class ProfileMoreActionButtom extends StatelessWidget {
+  final String description, subdescritpion;
+  final IconData icon;
+  const ProfileMoreActionButtom({
+    Key? key,
+    this.description = 'Lives in ',
+    this.subdescritpion = 'Goma.DRC',
+    this.icon = AntDesign.home,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: kGreyColor,
+          ),
+          const SizedBox(width: 10),
+          Text(
+            description,
+            style: themeData.textTheme.bodyText2,
+          ),
+          Text(
+            subdescritpion,
+            style: themeData.textTheme.bodyText2!.copyWith(color: kDoniel),
+          ),
+        ],
       ),
     );
   }
